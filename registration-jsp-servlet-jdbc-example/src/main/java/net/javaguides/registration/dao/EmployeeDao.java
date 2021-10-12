0e32f07ec4718b07353d5d79fe525267aafbdfca
ivanandika21 <ivanandikasurya21@gmail.com>
2021-10-12 23:18:11 +0700

    Upload dasaran TTS PL

---
+++ registration-jsp-servlet-jdbc-example/src/main/java/net/javaguides/registration/dao/EmployeeDao.java
@@ -0,0 +1,36 @@
+package net.javaguides.registration.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import net.javaguides.registration.model.Employee;
+
+public class EmployeeDao {
+	public int registerEmployee(Employee employee) throws ClassNotFoundException {
+		String INSERT_USERS_SQL = "INSERT INTO employee(id, first_name, last_name, username, password, address, contact) VALUES (?, ?, ?, ?, ?, ?, ?);";
+		
+		int result = 0;
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "admin");
+			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)){
+			preparedStatement.setInt(1, 1);
+			preparedStatement.setString(2, employee.getFirstName());
+			preparedStatement.setString(2, employee.getLastName());
+			preparedStatement.setString(2, employee.getUsername());
+			preparedStatement.setString(2, employee.getPassword());
+			preparedStatement.setString(2, employee.getAddress());
+			preparedStatement.setString(2, employee.getContact());
+			
+			System.out.println(preparedStatement);
+			result = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
---
+++ registration-jsp-servlet-jdbc-example/src/main/java/net/javaguides/registration/dao/Koneksi.java
@@ -0,0 +1,35 @@
+package net.javaguides.registration.dao;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+/**
+ *
+ * @author ivana
+ */
+public class Koneksi {
+    
+    public static Connection con;
+    public static Statement stm;
+    
+    public static void main(String args[]){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            con =DriverManager.getConnection("jdbc:mysql://localhost:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "admin");
+            stm = con.createStatement();
+            System.out.println("koneksi berhasil;");
+        } catch (Exception e) {
+            System.err.println("koneksi gagal" +e.getMessage());
+        }
+    }
+
+    static Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
