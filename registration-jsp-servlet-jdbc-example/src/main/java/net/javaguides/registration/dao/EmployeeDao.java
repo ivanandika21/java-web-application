7f5f0c36dc99a664e02dc71bd564fad9eb478bd1
ivanandika21 <ivanandikasurya21@gmail.com>
2021-10-13 22:28:01 +0700

    Update EmployeeDao.java

--- registration-jsp-servlet-jdbc-example/src/main/java/net/javaguides/registration/dao/EmployeeDao.java
+++ registration-jsp-servlet-jdbc-example/src/main/java/net/javaguides/registration/dao/EmployeeDao.java
@@ -19,11 +19,11 @@ public class EmployeeDao {
 			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)){
 			preparedStatement.setInt(1, 1);
 			preparedStatement.setString(2, employee.getFirstName());
-			preparedStatement.setString(2, employee.getLastName());
-			preparedStatement.setString(2, employee.getUsername());
-			preparedStatement.setString(2, employee.getPassword());
-			preparedStatement.setString(2, employee.getAddress());
-			preparedStatement.setString(2, employee.getContact());
+			preparedStatement.setString(3, employee.getLastName());
+			preparedStatement.setString(4, employee.getUsername());
+			preparedStatement.setString(5, employee.getPassword());
+			preparedStatement.setString(6, employee.getAddress());
+			preparedStatement.setString(7, employee.getContact());
 			
 			System.out.println(preparedStatement);
 			result = preparedStatement.executeUpdate();
